cfe249231c5e2c2a4524b6e7aace6e31d63d7362
thuongcjn <119556277+thuongcjn@users.noreply.github.com>
2025-08-04 09:57:59 +0700

    Update FetchName.jsx

--- client/src/requests/FetchName.jsx
+++ client/src/requests/FetchName.jsx
@@ -1,6 +1,6 @@
 export const fetchName = async (userId) => {
   const userData = await fetch(
-    `https://mern-blog-posts.onrender.com/api/users/${userId}`
+    `https://myapp-be.onrender.com/api/users/${userId}`
   );
   const { data } = await userData.json();
   return data?.userName;
